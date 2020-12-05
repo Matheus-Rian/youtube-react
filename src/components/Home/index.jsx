0bc1eda3398a5009f565efde43edb42fc1eab984
Matheus Rian <souzamatheusrian@gmail.com>
2020-12-05 00:40:59 -0200

    clone youtube-react

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -69,6 +69,88 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
+const videos = [
+  {
+    id: 1,
+    title:
+      'FEED DO USUÁRIO | Criando uma Rede Social com React.js e .NET Core #29',
+    channel: 'Lucas Nhimi',
+    views: '11 mi de visualizações',
+    date: 'há 1 semana',
+    avatar: "https://avatars3.githubusercontent.com/u/53922139?v=4",
+    thumb: 'https://miro.medium.com/max/8544/0*bQ2s1lDGEYzGjWVU',
+  },
+  {
+    id: 2,
+    title:
+      'COMO MELHORAR SEU CODIGO JAVASCRIPT (ESLINT + PRETTIER + EDITORCONFIG) | Dicas e Truques #02',
+    channel: 'Lucas Nhimi',
+    views: '957 mil visualizações',
+    date: 'há 1 semana',
+    avatar: "https://avatars3.githubusercontent.com/u/53922139?v=4",
+    thumb: 'https://miro.medium.com/max/8544/0*bQ2s1lDGEYzGjWVU',
+  },
+  {
+    id: 3,
+    title:
+      'CONTEXT API NO EDITOR DE POST | Criando uma Rede Social com React.js e .NET Core #27',
+    channel: 'Lucas Nhimi',
+    views: '106 mil visualizações',
+    date: 'há 1 semana',
+    avatar: "https://avatars3.githubusercontent.com/u/53922139?v=4",
+    thumb: 'https://miro.medium.com/max/8544/0*bQ2s1lDGEYzGjWVU',
+  },
+  {
+    id: 4,
+    title:
+      'CONTEXT API NO EDITOR DE POST | Criando uma Rede Social com React.js e .NET Core #27',
+    channel: 'Lucas Nhimi',
+    views: '5,6 mi de visualizações',
+    date: 'há 1 semana',
+    avatar: "https://avatars3.githubusercontent.com/u/53922139?v=4",
+    thumb: 'https://miro.medium.com/max/8544/0*bQ2s1lDGEYzGjWVU',
+  },
+  {
+    id: 5,
+    title:
+      'EDITOR DE POST COM MARKDOWN 2 | Criando uma Rede Social com React.js e .NET Core #26',
+    channel: 'Lucas Nhimi',
+    views: '2,2 mi de visualizações',
+    date: 'há 1 semana',
+    avatar: "https://avatars3.githubusercontent.com/u/53922139?v=4",
+    thumb: 'https://miro.medium.com/max/8544/0*bQ2s1lDGEYzGjWVU',
+  },
+  {
+    id: 6,
+    title: 'COMO MIGRAR PARA REACT HOOKS | Dicas e Truques #01',
+    channel: 'Lucas Nhimi',
+    views: '233 mil visualizações',
+    date: 'há 1 semana',
+    avatar: "https://avatars3.githubusercontent.com/u/53922139?v=4",
+    thumb: 'https://miro.medium.com/max/8544/0*bQ2s1lDGEYzGjWVU',
+  },
+  {
+    id: 7,
+    title:
+      'PRÉ-REQUISITOS | Criando uma Rede Social com React.js e .NET Core #01',
+    channel: 'Lucas Nhimi',
+    views: '118 mil visualizações',
+    date: 'há 1 semana',
+    avatar: "https://avatars3.githubusercontent.com/u/53922139?v=4",
+    thumb: 'https://miro.medium.com/max/8544/0*bQ2s1lDGEYzGjWVU',
+  },
+  {
+    id: 8,
+    title:
+      'GIT E GITHUB | Criando uma Rede Social com React.js e .NET Core #04',
+    channel: 'Lucas Nhimi',
+    views: '1,9 mi de visualizações',
+    date: 'há 1 semana',
+    avatar: "https://avatars3.githubusercontent.com/u/53922139?v=4",
+    thumb: 'https://miro.medium.com/max/8544/0*bQ2s1lDGEYzGjWVU',
+  },
+];
+
 function Home() {
   const classes = useStyles();
 
@@ -327,15 +409,15 @@ function Home() {
           </Typography>
 
           <Grid container>
-              <Grid item>
-                teste
-              </Grid>
-              <Grid item>
-                teste
-              </Grid>
-              <Grid item>
-                teste
-              </Grid>
+             {
+               videos.map((item, index) => (
+                 <Grid item lg={3} md={4} sm={6} xs={12}>
+                    <Box>
+                      
+                    </Box> 
+                 </Grid>
+               ))
+             }   
           </Grid>
         </Box>
       </Box>
